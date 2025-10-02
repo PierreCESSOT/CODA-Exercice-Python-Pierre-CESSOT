b840a2cc6a70ab83d2f10ab5487b7071fd0efc55
CESSOT <cessotp@gmail.com>
2025-10-02 13:42:54 +0200

    Exercice [1] terminé

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ def exercice1():
     print("Exercice 1 : Bonjour le monde !")
     print("Hello World !")
 
+def exercice2():
+    print("Exercice 2 : Afficher son prenom")
+    prenom = input("Entrez votre prénom : ")
+    print(f"Bonjour, {prenom} !")
+
+
+
 def main():
     # Demande à l'utilisateur quel exercice exécuter
     choix = input("Entrez le numéro de l'exercice à exécuter : ")
     if choix == "1":
         exercice1()
+    elif choix == "2":
+        exercice2()
     else:
         print("Exercice non reconnu.")
 if __name__ == "__main__":
